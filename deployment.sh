b89130108cf47e79814f8f9cc84bfe46759b3d62
EC2 Default User <ec2-user@ip-172-31-82-11.ec2.internal>
2019-06-14 03:26:51 +0000

    Completed the logic for DeepRacer call to the API.

--- deployment.sh
+++ deployment.sh
@@ -7,3 +7,8 @@ sam deploy \
     --template-file packaged.yaml \
     --stack-name deepracer-reward \
     --capabilities CAPABILITY_IAM
+    
+RewardFunctionApi=$(aws cloudformation describe-stacks --stack-name deepracer-reward \
+--query 'Stacks[0].Outputs[?OutputKey==`RewardFunctionApi`].OutputValue' --output text)
+
+sed "s~###APIEndpoint###~$RewardFunctionApi~g" reward.template > reward.py
--- reward/app.py
+++ reward/app.py
@@ -6,14 +6,34 @@ from sympy.geometry import *
 
 
 def lambda_handler(event, context):
+    params = json.loads(event["queryStringParameters"]["json"])
+    print(params)
+    
+    '''
+    Example of rewarding the agent to follow center line
+    '''
 
-    print(event)
-    x = Point(0, 0)
-    y = Point(1, 1)
-    z = Point(2, 2)
-    zp = Point(1, 0)
-    Point.is_collinear(x, y, z)
+    # Read input parameters
+    track_width = params['track_width']
+    distance_from_center = params['distance_from_center']
+    
+    # Calculate 3 markers that are at varying distances away from the center line
+    marker_1 = 0.1 * track_width
+    marker_2 = 0.25 * track_width
+    marker_3 = 0.5 * track_width
+    
+    # Give higher reward if the car is closer to center line and vice versa
+    if distance_from_center <= marker_1:
+        reward = 1.0
+    elif distance_from_center <= marker_2:
+        reward = 0.5
+    elif distance_from_center <= marker_3:
+        reward = 0.1
+    else:
+        reward = 1e-3  # likely crashed/ close to off track
 
+     
     return {
-        "value": 1
+        'statusCode': 200,
+        'body': json.dumps({"reward": float(reward)})
     }
